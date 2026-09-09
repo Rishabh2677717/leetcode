60567a0494919b9517e788a38558422bbda9b266
Rishabh2677717 <59829767+Rishabh2677717@users.noreply.github.com>
2026-09-10 00:04:25 +0530

    [1011] [Time Beats: -%] [Memory Beats: -%] - LeetPush

---
+++ 1011-Capacity-To-Ship-Packages-Within-D-Days.java
@@ -0,0 +1,42 @@
+class Solution {
+     public static boolean ship(int[] weights, int capacity, int days) {
+
+        int usedDays = 1;
+        int sum = 0;
+
+        for (int num : weights) {
+
+            if (sum + num <= capacity) {
+                sum += num;
+            } else {
+                usedDays++;
+                sum = num;
+            }
+        }
+
+        return usedDays <= days;
+    }
+
+    public int shipWithinDays(int[] weights, int days) {
+        int low = Integer.MIN_VALUE;        // maximum element
+        int high = 0;// total sum
+
+     
+
+        for (int num : weights) {
+            high += num;
+            low =Math.max(low,num);
+        }
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (ship(weights, mid, days)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+return low;
+    }
+}
